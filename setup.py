aea5a567594b902a3726f662851cf47da8aa9a83
BenSaintKoff <benoit.ipou@inphb.ci>
2020-03-02 23:32:39 +0000

    Saving of modification

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='idsi_pkg',
+    version='1.0.0',
+    author='benoit.ipou@inphb.ci',
+    description='Essai de création de package python',
+    packages=setuptools.find_packages(),
+    install_requires=[]
+)
